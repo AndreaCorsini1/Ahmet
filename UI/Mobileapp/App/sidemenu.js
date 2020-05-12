cbc164cf82516f5cf46e5f6cdf507bcd2933d520
Matte <matte@yoda.fritz.box>
2020-05-12 11:54:22 +0200

    12maggio

---
+++ UI/Mobileapp/App/sidemenu.js
@@ -0,0 +1,81 @@
+/*Example of Navigation Drawer with Sectioned Menu*/
+
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import {NavigationActions} from 'react-navigation';
+import {ScrollView, Text, View, StyleSheet} from 'react-native';
+//import Drawer from 'react-native-circle-drawer';
+
+class SideMenu extends Component {
+  constructor() {
+    super();
+    /*Array of the sidebar navigation option with
+    Heading, Subheading and screen to navigate.*/
+    //Sreen to navigate can be any screen defined in Drawer Navigator in App.js
+    this.options = [
+      {
+        mainHeading: 'Ahmet Menu',
+        subOptions: [
+          {secondaryHeading: 'Home', navigationPath: 'First'},
+          {secondaryHeading: 'New Study', navigationPath: 'Second'},
+          {secondaryHeading: 'Study', navigationPath: 'Third'},
+          {secondaryHeading: 'Statistics', navigationPath: 'Four'},
+        ],
+      },
+    ];
+  }
+
+  navigateToScreen = (route) => () => {
+    const navigateAction = NavigationActions.navigate({
+      routeName: route,
+    });
+    this.props.navigation.dispatch(navigateAction);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <View>
+            {this.options.map((option, key) => (
+              <View>
+                <Text style={styles.mainHeading}>{option.mainHeading}</Text>
+                {option.subOptions.map((item, key) => (
+                  <View style={styles.secondaryHeading} key={key}>
+                    <Text onPress={this.navigateToScreen(item.navigationPath)}>
+                      {item.secondaryHeading}
+                    </Text>
+                  </View>
+                ))}
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+        <View style={styles.footerContainer}>
+          <Text>Ahmet side menu</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    flex: 1,
+  },
+  secondaryHeading: {
+    padding: 10,
+  },
+  mainHeading: {
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    backgroundColor: '#B4DCF3',
+  },
+  footerContainer: {
+    padding: 20,
+    backgroundColor: '#B4DCF3',
+  },
+});
+
+export default SideMenu;
